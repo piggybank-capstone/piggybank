1ddc54569e28f8166f49afe663ffb22442773798
Marie Carter <marievcarter@gmail.com>
2019-01-24 18:05:40 -0500

    Add server files and connect backend to frontend.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+// import Plaid from 'plaid';
+
+// const plaidClient = new plaid.Client(
+//   process.env.PLAID_CLIENT_ID,
+//   process.env.PLAID_SECRET,
+//   process.env.PUBLIC_KEY,
+//   plaid.environments.sandbox,
+//   { version: '2018-05-22' }
+// );
 
 class App extends Component {
+  async componentDidMount() {
+    const { data } = await Axios.get('/api/users');
+    console.log('data:', data);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>piggybank</p>
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
         </header>
       </div>
     );
