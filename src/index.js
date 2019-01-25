6ffae0bc4e1aabf8061440cc63caca81d02e390a
Marie Carter <marievcarter@gmail.com>
2019-01-25 16:01:13 -0500

    Trying out recharts.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Trends from './Trends';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <App />
+    <Trends />
+  </div>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
