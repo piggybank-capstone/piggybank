e891dbf3cd09dec5b85c813ab134ea8113416fc4
Sara Yoon <sarayoon12@gmail.com>
2019-01-25 14:22:58 -0500

    fixed untracked files

--- secrets.js
+++
@@ -1,3 +0,0 @@
-process.env.PLAID_CLIENT_ID = '5c49e781d8717a0010e5173a';
-process.env.PLAID_SECRET_DEVELOPMENT = 'd28935c3230fc922ea33fb5528a2de';
-process.env.PUBLIC_KEY = 'b50d6c5ab6295ff1aca2a4b971fc7e';
