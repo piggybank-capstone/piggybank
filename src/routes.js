e88843bc8be9405b1d72f77e1a953ac0d1008fa8
Marie Carter <marievcarter@gmail.com>
2019-01-25 16:45:58 -0500

    adding router.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route, Switch } from 'react-router-dom';
-import {} from '.';
+import App from './App';
 
 /**
  * COMPONENT
@@ -10,19 +10,18 @@ class Routes extends Component {
   componentDidMount() {}
 
   render() {
-    const { isLoggedIn } = this.props;
-
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        {isLoggedIn && (
-          <Switch>
-            {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
+        {/* <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} /> */}
+         <Route path="/" component={}
+        {/*{isLoggedIn && (
+          <Switch> */}
+        {/* Routes placed here are only available after logging in */}
+        {/* <Route path="/home" component={UserHome} />
           </Switch>
-        )}
+        )} */}
         {/* Displays our Login component as a fallback */}
         <Route component={Login} />
       </Switch>
