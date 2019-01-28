1a0c8e19b6068c2e6a875a383322ed41c045cfcf
Marie Carter <marievcarter@gmail.com>
2019-01-28 17:36:25 -0500

    Working on new pie charts.

--- src/components/Trends.js
+++ src/components/Trends.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import '../App.css';
-import { LineChart, Line, PieChart, Pie, Tooltip, Cell } from 'recharts';
+import { PieChart, Pie, Sector, Tooltip, Cell } from 'recharts';
 
 class Trends extends Component {
   async componentDidMount() {
@@ -10,15 +10,50 @@ class Trends extends Component {
   }
 
   render() {
-    let recreation = this.props.transactions.filter(
-      item => item.category[0] === 'Recreation'
-    );
+    let recreation = this.props.transactions
+      .filter(item => item.category[0] === 'Recreation')
+      .reduce((total, num) => {
+        return total + parseFloat(num.amount);
+      }, 0);
     console.log(recreation);
+    const data = [
+      { name: 'recreation', value: recreation },
+      { name: 'Group B', value: 300 },
+      { name: 'Group C', value: 300 },
+      { name: 'Group D', value: 200 },
+    ];
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({
+      cx,
+      cy,
+      midAngle,
+      innerRadius,
+      outerRadius,
+      percent,
+      index,
+    }) => {
+      const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+      const x = cx + radius * Math.cos(-midAngle * RADIAN);
+      const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+      return (
+        <text
+          x={x}
+          y={y}
+          fill="white"
+          textAnchor={x > cx ? 'start' : 'end'}
+          dominantBaseline="central"
+        >
+          {`${(percent * 100).toFixed(0)}%`}
+        </text>
+      );
+    };
     return (
       <div className="App">
         <header className="App-header">
           <h3>Trends</h3>
-          <PieChart width={500} height={500}>
+          {/* <PieChart width={500} height={500}>
             <Pie
               name={
                 this.props.transactions.category
@@ -35,20 +70,22 @@ class Trends extends Component {
               label
             />
             <Tooltip />
-          </PieChart>
-          <PieChart width={500} height={500}>
+          </PieChart> */}
+          <PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
             <Pie
-              name="recreation"
-              isAnimationActive={true}
-              data={recreation}
-              dataKey="amount"
+              data={data}
               cx={300}
-              cy={300}
-              outerRadius={100}
+              cy={200}
+              labelLine={false}
+              //label={renderCustomizedLabel}
+              outerRadius={80}
+              fill="#8884d8"
+              label
             >
-              <Cell data={recreation} dataKey="amount" fill="#8884d8" />
+              {data.map((entry, index) => (
+                <Cell fill={COLORS[index % COLORS.length]} />
+              ))}
             </Pie>
-            <Tooltip />
           </PieChart>
         </header>
       </div>
