776dc6393621deb26ad2b31e2e237db76b5c24d6
k-westbrook <kcheriewestbrook@gmail.com>
2019-01-28 16:45:16 -0500

    google

--- server/auth/google.js
+++ server/auth/google.js
@@ -22,42 +22,44 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   console.log('Google client ID / secret not found. Skipping Google OAuth.')
 
 } else {
-
-  const googleConfig = {
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
-  }
-
-  const strategy = new GoogleStrategy(
-    googleConfig,
-    (token, refreshToken, profile, done) => {
-      const googleId = profile.id
-      const name = profile.displayName
-      const email = profile.emails[0].value
-
-      User.findOrCreate({
-        where: { googleId },
-        defaults: { email }
-      })
-        .then(([user]) => {
-          console.log(user, "USER GOOGLE");
-          done(null, user)
-        })
-        .catch(done)
+  try {
+    const googleConfig = {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK
     }
-  )
 
-  passport.use(strategy)
+    const strategy = new GoogleStrategy(
+      googleConfig,
+      (token, refreshToken, profile, done) => {
+        const googleId = profile.id
 
-  router.get('/', passport.authenticate('google', { scope: 'email' }))
+        const email = profile.emails[0].value
 
-  router.get(
-    '/callback',
-    passport.authenticate('google', {
-      successRedirect: '/home',
-      failureRedirect: '/login'
-    })
-  )
+        User.findOrCreate({
+          where: { googleId },
+          defaults: { email }
+        })
+          .then(([user]) => {
+            console.log(user, "USER GOOGLE");
+            done(null, user)
+          })
+          .catch(done)
+      }
+    )
 
+    passport.use(strategy)
+
+    router.get('/', passport.authenticate('google', { scope: 'email' }))
+
+    router.get(
+      '/callback',
+      passport.authenticate('google', {
+        successRedirect: '/home',
+        failureRedirect: '/login'
+      })
+    )
+  } catch (error) {
+    console.log("ERROR", error)
+  }
 }
