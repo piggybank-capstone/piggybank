bb36dcb6174a1730d9d66c8eb736d414fe29db5b
k-westbrook <kcheriewestbrook@gmail.com>
2019-01-28 21:31:16 -0500

    New Google API and updates to callback,etc

--- server/auth/google.js
+++ server/auth/google.js
@@ -20,46 +20,41 @@ module.exports = router
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   console.log('Google client ID / secret not found. Skipping Google OAuth.')
-
 } else {
-  try {
-    const googleConfig = {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK
-    }
-
-    const strategy = new GoogleStrategy(
-      googleConfig,
-      (token, refreshToken, profile, done) => {
-        const googleId = profile.id
-
-        const email = profile.emails[0].value
-
-        User.findOrCreate({
-          where: { googleId },
-          defaults: { email }
-        })
-          .then(([user]) => {
-            console.log(user, "USER GOOGLE");
-            done(null, user)
-          })
-          .catch(done)
-      }
-    )
-
-    passport.use(strategy)
-
-    router.get('/', passport.authenticate('google', { scope: 'email' }))
-
-    router.get(
-      '/callback',
-      passport.authenticate('google', {
-        successRedirect: '/home',
-        failureRedirect: '/login'
-      })
-    )
-  } catch (error) {
-    console.log("ERROR", error)
+  const googleConfig = {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
   }
+
+
+  const strategy = new GoogleStrategy(
+    googleConfig,
+    (token, refreshToken, profile, done) => {
+      const googleId = profile.id
+      let firstName = profile.name.givenName
+      let lastName = profile.name.familyName
+      const email = profile.emails[0].value
+
+      console.log("WHAT GOOGLE GOT BACK", profile)
+      User.findOrCreate({
+        where: { googleId },
+        defaults: { firstName, lastName, email }
+      })
+        .then(([user]) => done(null, user))
+        .catch(done)
+    }
+  )
+
+  passport.use(strategy)
+
+  router.get('/', passport.authenticate('google', { scope: 'email' }))
+
+  router.get(
+    '/callback',
+    passport.authenticate('google', {
+      successRedirect: 'http://localhost:3000/home',
+      failureRedirect: 'http://localhost:3000/login'
+    })
+  )
 }
