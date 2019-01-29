27b9c0f245a91ca60127cedca0689836dfbde09e
Rebecca Chan <chan.rebecca@gmail.com>
2019-01-28 21:56:15 -0500

    wrapped components with material ui. updated navbar color and logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,65 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logout } from '../store';
 import { NavLink } from 'react-router-dom';
+import { withTheme, MuiButton, MuiAppBar } from '@material-ui/core/styles';
+import Rectangle_Logo from './Rectangle_Logo.png';
+
+const styles = theme => ({
+  root: {
+    width: '100%'
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    }
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%'
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200
+    }
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex'
+    }
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
+  }
+});
 
 const NavBar = props => {
   const { isLoggedIn, handleClick } = props;
@@ -15,9 +69,7 @@ const NavBar = props => {
       <AppBar position="static">
         <Toolbar>
           <NavLink to={'/home'}>
-            <Typography variant="title" color="inherit">
-              piggybank
-            </Typography>
+            <img src={Rectangle_Logo} height="40" alt="piggybank logo" />
           </NavLink>
           {isLoggedIn ? (
             <Button color="inherit" onClick={handleClick}>
@@ -41,13 +93,13 @@ const NavBar = props => {
 };
 
 const mapState = state => ({
-  isLoggedIn: !!state.user.id,
+  isLoggedIn: !!state.user.id
 });
 
 const mapDispatch = dispatch => ({
   handleClick() {
     dispatch(logout());
-  },
+  }
 });
 
 export default connect(
--- src/index.js
+++ src/index.js
@@ -7,12 +7,46 @@ import store from './store';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import App from './App';
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  withTheme
+} from '@material-ui/core/styles';
+import { teal, blue } from '@material-ui/core/colors';
+import { colors } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: teal,
+    secondary: blue,
+    textColor: colors.white
+  },
+  status: {
+    danger: 'blue'
+  },
+  overrides: {
+    MuiButton: {
+      text: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)'
+      }
+    }
+  },
+  typography: { useNextVariants: true }
+});
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
+    <MuiThemeProvider theme={theme}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
