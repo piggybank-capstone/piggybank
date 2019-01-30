072daee5f9167d7c5f22414bf86cce15590d507d
Marie Carter <marievcarter@gmail.com>
2019-01-30 16:29:02 -0500

    Style accounts chart.

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,33 +1,59 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
 
 /* COMPONENT */
 class Accounts extends Component {
   render() {
-    const { accounts } = this.props;
+    const { accounts, classes } = this.props;
     return (
       <div>
         <h4>Accounts</h4>
-        <table>
-          <tr>
-            <th scope="row">Account</th>
-            <th scope="row">Official Name</th>
-            <th scope="row">Type</th>
-            <th scope="row">Available Balance</th>
-            <th scope="row">Current Balance</th>
-          </tr>
-          {accounts.map(account => {
-            return (
-              <tr key={account.id}>
-                <td>{account.name}</td>
-                <td>{account.official_name}</td>
-                <td>{account.subtype}</td>
-                <td>{account.balances.available}</td>
-                <td>{account.balances.current}</td>
-              </tr>
-            );
-          })}
-        </table>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableCell>Account</TableCell>
+              <TableCell align="right">Official Name</TableCell>
+              <TableCell align="right">Type</TableCell>
+              <TableCell align="right">Available Balance</TableCell>
+              <TableCell align="right">Current Balance</TableCell>
+            </TableHead>
+            <TableBody>
+              {accounts.map(account => {
+                return (
+                  <TableRow key={account.id}>
+                    <TableCell>{account.name}</TableCell>
+                    <TableCell align="right">{account.official_name}</TableCell>
+                    <TableCell align="right">{account.subtype}</TableCell>
+                    <TableCell align="right">
+                      {account.balances.available}
+                    </TableCell>
+                    <TableCell align="right">
+                      {account.balances.current}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Paper>
       </div>
     );
   }
@@ -39,4 +65,4 @@ const mapState = state => {
   };
 };
 
-export default connect(mapState)(Accounts);
+export default withStyles(styles)(connect(mapState)(Accounts));
