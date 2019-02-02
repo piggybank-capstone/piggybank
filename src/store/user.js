40a551f47be043bff2ce9c64e6afb2cf5acf5f2e
k-westbrook <kcheriewestbrook@gmail.com>
2019-02-02 12:53:20 -0500

    Sign up form and auth;

--- src/store/user.js
+++ src/store/user.js
@@ -31,13 +31,25 @@ export const me = () => async dispatch => {
 };
 
 // the methods are signup or login
-export const auth = (email, password, method) => async dispatch => {
-  let res;
+export const auth = (
+  email,
+  password,
+  method,
+  firstName,
+  lastName
+) => async dispatch => {
+  let res
   try {
-    res = await axios.post(`/auth/${method}`, {
-      email,
-      password,
-    });
+    if (method === 'signup') {
+      res = await axios.post(`/auth/${method}`, {
+        email,
+        password,
+        firstName,
+        lastName
+      })
+    } else {
+      res = await axios.post(`/auth/${method}`, { email, password })
+    }
   } catch (authError) {
     return dispatch(getUser({ error: authError }));
   }
