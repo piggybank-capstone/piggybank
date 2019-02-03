96696d9543eb3285a13dc63a1fde107ff817f140
Rebecca Chan <chan.rebecca@gmail.com>
2019-02-02 23:07:06 -0500

    restructured Routes and centered Plaid button

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,32 +3,13 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import NavBar from './NavBar';
 
-export const LandingPage = props => {
+const LandingPage = () => {
   return (
     <div>
-      {props.isLoggedin ? (
-        <NavBar />
-      ) : (
-        <div>
-          <NavBar />
-          <div id="header"> We Make Saving Fun</div>
-          <h2>Manage your finances and budget with Piggybot.</h2>
-        </div>
-      )}
-      ;
+      <div id="header"> We Make Saving Fun</div>
+      <h2>Manage your finances and budget with Piggybot.</h2>
     </div>
   );
 };
 
-const mapState = state => {
-  return {
-    isLoggedin: !!state.user.id
-  };
-};
-
-export default withRouter(
-  connect(
-    mapState,
-    null
-  )(LandingPage)
-);
+export default LandingPage;
