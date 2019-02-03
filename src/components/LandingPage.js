5e77c803cfc287e429ac3bad5ef0a99db1383bcd
Marie Carter <marievcarter@gmail.com>
2019-02-02 23:24:07 -0500

    Updated landing page to resolve prior merge conflicts.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,29 +6,10 @@ import NavBar from './NavBar';
 const LandingPage = () => {
   return (
     <div>
-      {props.isLoggedin ? (
-        <NavBar />
-      ) : (
-        <div>
-          <NavBar />
-          <div id="header"> We Make Saving Fun</div>
-          <h2>Manage your finances and budget with us!</h2>
-        </div>
-      )}
-      ;
+      <div id="header"> We Make Saving Fun</div>
+      <h2>Manage your finances and budget with Piggybot.</h2>
     </div>
   );
 };
 
-const mapState = state => {
-  return {
-    isLoggedin: !!state.user.id,
-  };
-};
-
-export default withRouter(
-  connect(
-    mapState,
-    null
-  )(LandingPage)
-);
+export default LandingPage;
