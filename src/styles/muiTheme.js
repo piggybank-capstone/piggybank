26352cff1197df93d06d4c945adba0278ef1eaf4
Rebecca Chan <chan.rebecca@gmail.com>
2019-02-04 16:27:30 -0500

    muiTheme update

--- src/styles/muiTheme.js
+++ src/styles/muiTheme.js
@@ -16,7 +16,7 @@ export const theme = createMuiTheme({
 
 theme.overrides.MuiButton = {
   root: {
-    fullWidth: true
+    // fullWidth: true
   },
   text: {
     [theme.breakpoints.down('sm')]: {
