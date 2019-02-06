2b1f68c9409513484dd97659757fe6eecd76f05b
Marie Carter <marievcarter@gmail.com>
2019-02-06 12:32:14 -0500

    Update images.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import money from './../images/icons.png';
 import responsive from './../images/responsive.png';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import money from './../images/icons.png';
+import pie from './../images/pie.png';
+import progress from './../images/progress.png';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
   },
   element: {
-    padding: theme.spacing.unit * 2,
     textAlign: 'center',
     color: theme.palette.text.primary,
   },
   mainImage: {
-    padding: theme.spacing.unit * 2,
     textAlign: 'center',
     color: theme.palette.text.primary,
-    maxWidth: '50%',
+    width: '30%',
+  },
+  card: {
+    maxWidth: 150,
+  },
+  media: {
+    // ⚠️ object-fit is not supported by IE 11.
+    objectFit: 'cover',
+  },
+  spacing: {
+    height: '5%',
   },
 });
 
@@ -26,8 +40,11 @@ const LandingPage = props => {
   return (
     <div className={classes.root}>
       <Grid container spacing={24}>
+        <Grid item xs={12} className={classes.elements}>
+          <div className={classes.spacing} />
+        </Grid>
         <Grid item xs={12} className={classes.element}>
-          <img src={money} alt="money icons" />
+          <img src={money} className={classes.mainImage} alt="money icons" />
         </Grid>
         <Grid item xs={12}>
           <div className={classes.element}>
@@ -41,20 +58,53 @@ const LandingPage = props => {
         </Grid>
         <Grid item xs={4}>
           <div className={classes.element}>
-            {/* <img src={pie} alt="pie chart" /> */}
-            visualize your spending habits.
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  className={classes.media}
+                  height="140"
+                  image={pie}
+                  title="Contemplative Reptile"
+                />
+                <CardContent>visualize your spending habits.</CardContent>
+              </CardActionArea>
+            </Card>
           </div>
         </Grid>
         <Grid item xs={4}>
           <div className={classes.element}>
-            {/* <img src={pig} alt="piggybot" /> */}
-            set goals with the help of piggybot!
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  className={classes.media}
+                  height="140"
+                  image={pie}
+                  title="Contemplative Reptile"
+                />
+                <CardContent>set goals with the help of piggybot!</CardContent>
+              </CardActionArea>
+            </Card>
           </div>
         </Grid>
         <Grid item xs={4}>
           <div className={classes.element}>
-            {/* <img src="https://via.placeholder.com/100" alt="progress bar" /> */}
-            see progress on your budgets.
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  className={classes.media}
+                  height="140"
+                  image={progress}
+                  title="Contemplative Reptile"
+                />
+                <CardContent>see progress on your budgets.</CardContent>
+              </CardActionArea>
+            </Card>
           </div>
         </Grid>
         <Grid item xs={10}>
