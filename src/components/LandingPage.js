85441f204eb361d2f326f4c407b3304d3b28e5b2
Marie Carter <marievcarter@gmail.com>
2019-02-05 22:26:41 -0500

    Add folder for component images. Layout material ui grid for landing page.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,70 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import pig from './../images/pig.png';
 
-const LandingPage = () => {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  element: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.primary,
+    border: 1,
+  },
+});
+
+const LandingPage = props => {
+  const { classes } = props;
+  console.log(pig);
   return (
-    <div>
-      <div id="header">
-        <h2 id="title">We Make Saving Fun</h2>
-      </div>
-      <h2>Manage your finances and budget with Piggybot.</h2>
+    // <div id="header">
+    //   <h2 id="title">We Make Saving Fun</h2>
+    // </div>
+    // <h2>Manage your finances and budget with Piggybot.</h2>
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <div className={classes.element}>
+            <img src="https://via.placeholder.com/200" alt="cash" />
+            <img src="https://via.placeholder.com/200" alt="piggybank" />
+            <img src="https://via.placeholder.com/200" alt="coins" />
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className={classes.element}>
+            <h2>we make saving fun.</h2>
+          </div>
+        </Grid>
+        <Grid item xs={10}>
+          <div className={classes.element}>
+            <hr />
+          </div>
+        </Grid>
+        <Grid item xs={4}>
+          <div className={classes.element}>visualize your spending habits.</div>
+        </Grid>
+        <Grid item xs={4}>
+          <div className={classes.element}>
+            set goals with the help of piggybot!
+          </div>
+        </Grid>
+        <Grid item xs={4}>
+          <div className={classes.element}>see progress on your budgets.</div>
+        </Grid>
+        <Grid item xs={10}>
+          <div className={classes.element}>
+            <hr />
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className={classes.element}>responsive image</div>
+        </Grid>
+      </Grid>
     </div>
   );
 };
 
-export default LandingPage;
+export default withStyles(styles)(LandingPage);
